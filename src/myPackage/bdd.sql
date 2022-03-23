08f9851157d90eb1adbfca29d974652aab167219
eaglelamlol <jjacque10@gmail.com>
2022-03-24 00:59:47 +0100

    sql

--- src/myPackage/bdd.sql
+++ src/myPackage/bdd.sql
@@ -11,27 +11,31 @@ USE customer;
 # Table: Customer
 #------------------------------------------------------------
 
+
 CREATE TABLE Customer(
                          Username       	Varchar (50)    NOT NULL ,
                          Password      		Varchar (50)	NOT NULL ,
                          BMI 			    double,
 
-    CONSTRAINT Customer_PK PRIMARY KEY (Username) FOREIGN KEY (Id_medication)
+    CONSTRAINT Customer_PK PRIMARY KEY (Username)
 )ENGINE=InnoDB;
 
 CREATE TABLE Medication(
                          Username       	Varchar (50)    NOT NULL ,
+                         Id					int NOT NULL,
                          Dosage      		int,
                          Time_ 			    Varchar(50),
                          Medication      	Varchar (50),
                          Id_medication      int             NOT NULL ,
+                         Date_ 				Varchar(50),
 
-    CONSTRAINT Medication_PK PRIMARY KEY (Username)
+    CONSTRAINT Medication_PK PRIMARY KEY (id), FOREIGN KEY (Username) REFERENCES Customer(Username)
 )ENGINE=InnoDB;
 
 
 
 INSERT INTO Customer VALUES("Victor", "Password",19.50);
 
-INSERT INTO Medication VALUES("Victor",5,"matin","doliprane",1);
-INSERT INTO Medication VALUES("Victor",3,"matin","aspirine",1);
+INSERT INTO Medication VALUES("Victor",0,5,"matin","doliprane",1,"24/03/2021");
+INSERT INTO Medication VALUES("Victor",1,3,"matin","aspirine",1,"20/02/2021");
+
